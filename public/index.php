14d325f8277bdb79ca8789119580d3bbe492d17c
Magomogo <mgnatenko@gmail.com>
2013-04-08 16:03:49 +0700

    Modified according to changes of Translator app.

--- public/index.php
+++ public/index.php
@@ -12,22 +12,23 @@ use Doctrine\CouchDB\HTTP\SocketClient as HttpClient;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+define('LOCALE', 'ru_RU');
+
 $app = translator();
 
 echo $app->translate('hello');
-echo '<hr>';
-echo $app->injectAtClientSide('ru_RU');
+echo $app->injectAtClientSide(strtolower(LOCALE));
 
 //--------------------------------------------------------------------------------------------------
 
 function translator() {
-    $storage = new Translator\Storage\CouchDb(new CouchDBClient(new HttpClient(), 'ru_RU'));
+    $storage = new Translator\Storage\CouchDb(new CouchDBClient(new HttpClient(), strtolower(LOCALE)));
     $mode = array_key_exists('t', $_GET) ?
         Translator\Application::TRANSLATE_ON: Translator\Application::TRANSLATE_OFF;
 
     return new Translator\Application(
         '/translator/',
-        new \Translator\Adapter\Simple($storage->readTranslations(), $mode),
+        new \Translator\Adapter\Simple($storage->mappedTranslations(), $mode),
         $mode
     );
 }
