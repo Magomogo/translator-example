1cfceb6482c1ae326370d0251a658938ae65477a
Magomogo <mgnatenko@gmail.com>
2013-08-09 15:11:32 +0700

    added message context description to be imported with translations

---
+++ script/translations/context.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'hello' => 'Global greeting as a separate sentence',
+    'plural' => array(
+        'numberOfPages' => 'Amount of pages in total, ICU-formatted message with one parameter.'
+    ),
+    'niceDate' => array(
+        'time' => array(
+            'hours' => 'Amount of hours, to be part of some message. ICU-formatted with single parameter.',
+            'minutes' => 'Amount of minutes, to be part of some message. ICU-formatted with single parameter.'
+        )
+    )
+);
